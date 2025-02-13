1e6e3726efbaaabf209829d9c88b66c91dff9561
mira <mira>
2025-02-13 11:12:00 +0100

    header , make it dynamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 export default function Header() {
+    const [pageState, setPageState] = useState("Sign in")
     const location = useLocation();
     const navigate = useNavigate()
+    const auth = getAuth();
 
-    function pathMathRoute(route) {
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setPageState("Profile")
+            } else {
+                setPageState("Sign in")
+            }
+        })
+        return () => unsubscribe()
+    }, [auth])
+
+    function pathMatchRoute(route) {
         return route === location.pathname;
 
         /*  if (route === location.pathname) {
@@ -26,20 +40,23 @@ export default function Header() {
                 </div>
                 <div className='pr-8'>
                     <ul className='flex space-x-10'>
-                        <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent
-                            ${pathMathRoute("/") && "text-black border-b-red-500"} `}
+                        <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent transition duration-300
+                            ${pathMatchRoute("/") && "text-black border-b-red-500"}   hover:border-b-red-500 hover:text-black`}
                             onClick={() => navigate("/")}
                         >Home</li>
 
-                        <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent
-                            ${pathMathRoute("/offers") && "text-black border-b-red-500"} `}
+                        <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent  transition duration-300
+                            ${pathMatchRoute("/offers") && "text-black border-b-red-500"}   hover:border-b-red-500 hover:text-black`}
                             onClick={() => navigate("/offers")}
                         >Offers</li>
 
-                        <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent
-                            ${pathMathRoute("/sign-in") && "text-black border-b-red-500"} `}
-                            onClick={() => navigate("/sign-in")}
-                        >Sign in</li>
+                        <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent  transition duration-300
+                            ${(pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) && "text-black border-b-red-500"} hover:border-b-red-500 hover:text-black `}
+                            onClick={() => navigate("/profile")}
+
+                        >
+                            {pageState}
+                        </li>
                     </ul>
                 </div>
             </header>
